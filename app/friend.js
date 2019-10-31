384bffedeabbf83c55dabe04cfe2b1e39f7db702
Natalie Montano <nat.montano366@gmail.com>
2019-10-31 10:12:53 -0700

    created a friends list with characters from favorite show

---
+++ app/friend.js
@@ -0,0 +1,57 @@
+// Array that holds data objects, a list of all friends and applicable data
+
+// Might need to change the datatype of answers to string
+module.exports = [
+	{
+		name: "Barney Stinson",
+		photo: "https://media.giphy.com/media/12BpedC0YjOT8k/giphy.gif",
+		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+	},
+	{
+		name: "Robin Scherbatsky",
+		photo: "https://media.giphy.com/media/W825lDYRuIbHa/giphy.gif",
+		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Marshall Eriksen",
+		photo: "https://media.giphy.com/media/fwCcWH8kQ6leU/giphy.gif",
+		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Lily Aldrin",
+		photo: "https://media.giphy.com/media/z52VqCTTIXmg0/giphy.gif",
+		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: " Ted Mosby",
+		photo: "https://media.giphy.com/media/Dsw43rMfWj4hG/giphy.gif",
+		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+	},
+
+	{
+		name: "Patrice",
+		photo: "https://media.giphy.com/media/3o7WIrzMSMQxU6oyYg/giphy.gif",
+		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+	},
+	{
+		name: "Zoey Pierson",
+		photo: "https://68.media.tumblr.com/318eb63aa2a12b9ad918c5caa64a32fc/tumblr_o0nn9bAT6e1rcf8iro5_250.gif",
+		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
+	},
+	{
+		name: "Stella Zinman",
+		photo: "https://24.media.tumblr.com/tumblr_lro8mkyaFD1qhng3xo4_250.gif",
+		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+	},
+	{
+		name: "The Captain",
+		photo: "https://media.giphy.com/media/MS1JkGTXIeZi/giphy.gif",
+		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+	},
+ 
+
+];
+
+// module.exports.push2arr = function(data) {
+// 	module.exports.allFriends.push(data);
+// }
